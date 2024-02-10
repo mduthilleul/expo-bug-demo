943d5376a7694fee9e86fa03fb3de865d5ae8322
Michaël Duthilleul <michael.duthilleul@gmail.com>
2024-02-10 19:07:07 +0100

    Setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Image resizeMode='contain' src={require('../../assets/images/icon.png')} style={{width: 200, height: 200}} />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
